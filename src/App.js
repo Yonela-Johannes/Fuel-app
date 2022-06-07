0d6abdec6c4bf52be74da64ca97edee2be2c9596
Yonela-Johannes <johannesyonela@gmail.com>
2022-06-08 00:50:15 +0200

    setting up deployment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { HomeScreen } from './screens/HomeScreen';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
