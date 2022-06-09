e5e83f8f15a7d0125aa22f3d639f0ec1ec8fdb55
Yonela-Johannes <johannesyonela@gmail.com>
2022-06-09 02:26:46 +0200

    page styling implementation

---
+++ Js/app.js
@@ -0,0 +1,10 @@
+const dropdownItem = document.querySelectorAll('.dropdown-item')
+// init dropdown selection menu
+const count = document.querySelector('.count')
+const fuel = ['Petrol', 'Diesel']
+const selected = 'Select Fuel'
+
+count.innerHTML = 0
+
+console.log(dropdownItem)
+
