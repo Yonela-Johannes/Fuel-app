bc9b6df39e720922db8af920136af15c896cb386
Yonela-Johannes <johannesyonela@gmail.com>
2022-06-10 03:52:49 +0200

    getting data to charts

--- Js/chart.js
+++ Js/chart.js
@@ -1,87 +1,107 @@
+const date = new Date()
+const day = date.getDay()
+
 export let user =
 {
     name: 'Linda',
     image: '../img/linda.png',
     age: 30,
     location: 'Limpopo',
+    enteredAmount: 0,
+    totalLitres: 0,
+    lastWeekTotalLitres: 0,
+    lastWeekTotalAmount: 0,
+    change: 0,
+    fuel: '',
+    fuelPrice: [0, 0, 0, 0, 0, 0, 0],
     weeklyData: [{
         'Monday': {
-            'litres': 10.23,
-            "amount": 100.25,
-            'previousWeek': 50.10,
+            'litres': 0,
+            'previousWeekLitres': 0,
+            "amount": 0,
+            'previousWeek': 0,
         },
         'Tuesday': {
-            'litres': 15.23,
-            "amount": 190.25,
-            'previousWeek': 50.10,
+            'litres': 0,
+            'previousWeekLitres': 0,
+            "amount": 0,
+            'previousWeek': 0,
         },
         'Wednesday': {
-            'litres': 5,
-            "amount": 50,
-            'previousWeek': 100.10,
+            'litres': 0,
+            'previousWeekLitres': 0,
+            "amount": 0,
+            'previousWeek': 0,
         },
         'Thursday': {
-            'litres': 7,
-            "amount": 400,
-            'previousWeek': 200.10,
+            'litres': 0,
+            'previousWeekLitres': 0,
+            "amount": 0,
+            'previousWeek': 0,
         },
         'Friday': {
             'litres': 1.23,
-            "amount": 10.25,
-            'previousWeek': 100.50,
+            'previousWeekLitres': 0,
+            "amount": 0,
+            'previousWeek': 0,
         },
         'Saturday': {
-            'litres': 3.5,
-            "amount": 36.25,
-            'previousWeek': 50.50,
+            'litres': 0,
+            'previousWeekLitres': 0,
+            "amount": 0,
+            'previousWeek': 0,
         },
         'Sunday': {
-            'litres': 10.23,
-            "amount": 136.25,
+            'litres': 0,
+            'previousWeekLitres': 0,
+            "amount": 0,
             'previousWeek': 0,
         }
     }],
 }
 
-user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : localStorage.setItem('user', JSON.stringify(user))
+user.fuelPrice[day - 1] = user.fuelPrice[day - 1] == 0 ? 19.25 : user.fuelPrice[day - 1]
 
 
-const week = Object.keys(user.weeklyData[0])
+const week = Object.keys(user.weeklyData[0]) ? Object.keys(user.weeklyData[0]) : []
 // getting litres
-
+const previousWeekLitres = () => Object.values(user.weeklyData[0]).map(elem => elem.previousWeekLitres)
 const weeklyLitres = () => Object.values(user.weeklyData[0]).map(elem => elem.litres)
-// console.log(weeklyLitres())
 const getTotalLitres = () => Object.values(user.weeklyData[0]).map(elem => elem.litres).reduce((total, acc) => total + acc)
-// console.log(getTotalLitres())
 const previousWeekAmounts = () => Object.values(user.weeklyData[0]).map(elem => elem.previousWeek)
 const weeklyAmounts = () => Object.values(user.weeklyData[0]).map(elem => elem.amount)
-// console.log(weeklyAmounts())
 const getTotalAmount = () => Object.values(user.weeklyData[0]).map(elem => elem.amount).reduce((total, acc) => total + acc)
-// console.log(getTotalAmount())
-
-
+user.lastWeekTotalLitres = getTotalLitres()
+user.lastWeekTotalAmount = getTotalAmount()
+user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : localStorage.setItem('user', JSON.stringify(user))
 
 // getting amount
 
 
 // Monthly Chart
-const litersFuel = {
+const litresFuel = {
     labels: week,
     datasets: [{
         label: 'last Week',
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
+        data: previousWeekLitres()
+    }, {
+        label: 'Current Week',
+        backgroundColor: 'green',
         data: weeklyLitres(),
-    }]
+    }
+
+    ]
 };
 
 const monthlyConfig = {
-    type: 'line',
-    data: litersFuel,
+    type: 'bar',
+    data: litresFuel,
     options: {}
 };
 
-const myMonthlyChart = new Chart(
+const myLitresChart = new Chart(
     document.getElementById('monthlyFuelChartForecast'),
     monthlyConfig
 );
@@ -117,33 +137,24 @@ const myChart = new Chart(
 );
 
 // Changes in Fuel
-// const fuelLabels = [
-//     'Monday',
-//     'Tuesday',
-//     'Wednesday',
-//     'Thursday',
-//     'Friday',
-//     'Saturday',
-//     'Sunday',
-// ];
 
-// const fuelData = {
-//     labels: fuelLabels,
-//     datasets: [{
-//         label: 'My First dataset',
-//         backgroundColor: 'rgb(255, 99, 132)',
-//         borderColor: 'rgb(255, 99, 132)',
-//         data: [0, 10, 5, 2, 20, 30, 45],
-//     }]
-// };
+const fuelData = {
+    labels: week,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: user.fuelPrice,
+    }]
+};
 
-// const fuelConfig = {
-//     type: 'bar',
-//     data: fuelData,
-//     options: {}
-// };
+const fuelConfig = {
+    type: 'bar',
+    data: fuelData,
+    options: {}
+};
 
-// const fuelChart = new Chart(
-//     document.getElementById('changesInFuelForecast'),
-//     config
-// );
+const fuelChart = new Chart(
+    document.getElementById('changesInFuelForecast'),
+    fuelConfig
+);
--- Js/main.js
+++ Js/main.js
@@ -5,35 +5,56 @@ const date = new Date()
 const day = date.getDay()
 
 let user = JSON.parse(localStorage.getItem('user'))
-console.log(day)
-console.log(user)
 const enterFuelAmount = document.querySelector('.entered-fuel-amount')
 const amountValue = document.querySelector('.input-amount')
 
 const fuelPriceElem = document.querySelector('.fuel-price')
 const calculateButton = document.querySelector('.calc')
 const save = document.querySelector('.save')
+const currentFuelPrice = document.querySelector('.current-fuel-price')
 const calcLitres = document.querySelector('.calc-litres')
 const calcAmount = document.querySelector('.calc-amount')
 const litresAmount = document.querySelector('.litres-amount')
 const calcChange = document.querySelector('.calc-change')
+const storedData = document.querySelector('.stored-data')
+const weeklyData = document.querySelector('.weekly-data')
 
 const defaultAmount = 0
-fuelPriceElem.innerHTML = `Entered Amount: <span class='lit'>R ${defaultAmount.toFixed(2)}<span> `
-calcLitres.innerHTML = `Estimated Litres: <span class='lit'>${defaultAmount} litres<span> `
+console.log(user)
+storedData.innerHTML = `
+    <div class='name'>Name: ${user.name}</div>
+    <div class='age'>age: ${user.age}</div>
+    <div class='location'>From: ${user.location}</div>
+    <div class='calc-details'>
+    ${user.lastWeekTotalAmount.toFixed(2)}
+    </div>
+    `
+weeklyData.innerHTML = `
+
+    <div class='location'>Last Week Total Amount</div>
+    <div class='rand'>R ${user.lastWeekTotalAmount.toFixed(2)}</div>
+    <div class='location'>Last Week Total litres</div>
+    <div class='rand'>${user.lastWeekTotalLitres.toFixed(2)} litres</div>
+
+    `   
+
+currentFuelPrice.innerHTML = `Current price: <span class='lit'>R ${user.fuelPrice[day - 1].toFixed(2)}<span> `
+fuelPriceElem.innerHTML = `Entered Amount: <span class='lit'>R ${user.enteredAmount.toFixed(2)}<span> `
+calcLitres.innerHTML = `Estimated Litres: <span class='lit'>${defaultAmount.toFixed(1)} litres<span> `
 calcAmount.innerHTML = `
     <div class='cost-title'>Cost of litres</div>
- <div class='lit-container'>Amount: <span class='rand'>R ${defaultAmount.toFixed(2)}<span></div> `
-calcChange.innerHTML = `change: <span class='lit'>R ${defaultAmount.toFixed(2)}<span> `
+ <div class='lit-container'>Amount: <span class='rand'>R ${user.totalLitres}<span></div> `
+calcChange.innerHTML = `change: <span class='lit'>R ${user.change.toFixed(2)}<span> `
 
 const calculateFuel = () => {
-    const amount = amountValue.value
+    const amount = amountValue.value ? amountValue.value : user.fuelPrice[day - 1].toFixed(2)
     const enteredAmount = enterFuelAmount.value
     const enteredLitres = litresAmount.value
 
     fuelforecast.setFuelPrice(amount)
     fuelforecast.setPrice(enteredAmount)
     fuelforecast.fuelCosLitres(enteredLitres)
+
 
 
     fuelPriceElem.innerHTML = `Entered Amount: <span class='lit'>R ${fuelforecast.getPrice()}<span> `
@@ -48,11 +69,16 @@ const calculateFuel = () => {
 }
 
 const saveData = () => {
+    const amount = amountValue.value
+    const enteredAmount = enterFuelAmount.value
     const enteredLitres = litresAmount.value
+
+    fuelforecast.setPrice(enteredAmount)
     const users = JSON.parse(localStorage.getItem('user'))
-    users.estimatedLitres.push(parseFloat(fuelforecast.fuelCost()))
-    users.estimatedAmount.push(parseFloat(fuelforecast.fuelCosLitres(enteredLitres)))
-    console.log(users)
+    users.fuelPrice[day] = fuelforecast.getFuelPrice()
+    Object.values(users.weeklyData[0])[day - 1].litres = fuelforecast.fuelCost()
+
+    user.fuelPrice[day - 1] = fuelforecast.getFuelPrice()
     localStorage.setItem('user', JSON.stringify(users))
 }
 
